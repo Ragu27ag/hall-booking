1fc15fb2d106c55300066c20f8e504b0c77113cd
ragunath <raguyuvi27@gmail.com>
2023-07-06 18:16:54 +0530

    added apis

--- crud.js
+++ crud.js
@@ -1,12 +1,121 @@
 
+// let room = []
+// let book = []
+let room = [{
+    seats: "3",
+    amenities: ["swimming pool", "AC", "restaurent"],
+    Pricehr: "1000",
+    roomid: "1",
+    status: "booked"
+},
+{
+    seats: "5",
+    amenities: ["swimming pool", "AC", "restaurent"],
+    Pricehr: "10000",
+    roomid: "3",
+    status: "booked"
+}
+]
+let book = [{
+    custname: "AGR",
+    date: "06.07.2023",
+    starttime: "12:00PM",
+    endtime: "12:00PM",
+    roomid: "1"
+},
+{
+    custname: "AGR",
+    date: "06.07.2023",
+    starttime: "12:00PM",
+    endtime: "12:00PM",
+    roomid: "4"
+}]
 
 
-var room = []
+
+const readRoom = () =>
+    (JSON.stringify(room))
+
+const readBook = () =>
+    (JSON.stringify(book))
+
 
 const createRoom = (roomobj) => {
-    room = [...roomobj]
+    room.push(roomobj)
+}
+
+const roomBooking = (bookRoom) => {
+    book.push(bookRoom)
+}
+
+const roomDetails = () => {
+    var d = {}
+
+    var a = []
+    for (var i = 0; i < room.length; i++) {
+        a[i] = book.map((obj) => {
+
+            if (obj.roomid === room[i].roomid) {
+
+                return (
+                    d = {
+                        'id': room[i].roomid,
+                        'status': room[i].status,
+                        'name': obj.custname,
+                        'date': obj.date,
+                        'starttime': obj.starttime,
+                        'endtime': obj.endtime
+                    }
+                )
+
+            }
+
+
+
+        })
+
+    }
+
+
+
+    return (JSON.stringify(a))
+
+}
+
+const cusDetails = () => {
+    var e = {}
+
+    var f = []
+    for (var i = 0; i < room.length; i++) {
+        f[i] = book.map((obj) => {
+
+            if (obj.roomid === room[i].roomid) {
+
+                return (
+                    e = {
+                        'id': room[i].roomid,
+                        
+                        'name': obj.custname,
+                        'date': obj.date,
+                        'starttime': obj.starttime,
+                        'endtime': obj.endtime
+                    }
+                )
+
+            }
+
+
+
+        })
+
+    }
+
+
+
+    return (JSON.stringify(f))
+
 }
 
 
 
-export {createRoom}
+export { createRoom, readRoom, roomBooking, readBook, roomDetails,cusDetails }
--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 import express from  'express';
 
-import { createRoom } from './crud.js';
+import { createRoom, cusDetails, readBook, readRoom, roomBooking, roomDetails } from './crud.js';
 
 
 const app = express();
 const port = 5000;
 const host = 'localhost';
 
+app.use(express.json());
+
+app.get('/room',(req,res) => {
+   res.send(readRoom())
+})
+
+app.get('/room/book',(req,res) => {
+   res.send(readBook())
+})
 
 app.post('/room',(req,res) =>{
     const roomobj = req.body
-    res.send(createRoom(roomobj))
+    console.log(roomobj)
+    createRoom(roomobj)
+     res.send('created')
+})
+
+app.post('/room/book' , (req,res) => {
+   const bookRoom = req.body
+   console.log(bookRoom)
+   roomBooking(bookRoom)
+   res.send('created')
+})
+
+app.get('/room/details', (req,res) => {
+   res.send(roomDetails())
+})
+
+
+app.get('/room/cusdetails',(req,res) => {
+   res.send(cusDetails())
 })
 
 
 
-
 app.listen(port,() =>{
-   
+   console.log(`http://${host}:${port}`)
 })
